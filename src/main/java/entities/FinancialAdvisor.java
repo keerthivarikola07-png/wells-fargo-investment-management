90ffb0022291bc52946bc40871125729ab35bfbc
keerthivarikola07-png <keerthivarikola07@gmail.com>
2026-06-16 23:22:05 +0530

    Update FinancialAdvisor.java

--- src/main/java/entities/FinancialAdvisor.java
+++ src/main/java/entities/FinancialAdvisor.java
@@ -1,1 +1,72 @@
+package com.wellsfargo.entities;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class FinancialAdvisor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long advisorId;
+
+    @Column
+    private String advisorName;
+
+    @Column
+    private String email;
+
+    @Column
+    private String phoneNumber;
+
+    @OneToMany(mappedBy = "advisor")
+    private List<Client> clients;
+
+    public FinancialAdvisor(Long advisorId, String advisorName,
+                            String email, String phoneNumber,
+                            List<Client> clients) {
+        this.advisorId = advisorId;
+        this.advisorName = advisorName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.clients = clients;
+    }
+
+    public FinancialAdvisor() {}
+
+    public Long getAdvisorId() {
+        return advisorId;
+    }
+
+    public String getAdvisorName() {
+        return advisorName;
+    }
+
+    public void setAdvisorName(String advisorName) {
+        this.advisorName = advisorName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public List<Client> getClients() {
+        return clients;
+    }
+
+    public void setClients(List<Client> clients) {
+        this.clients = clients;
+    }
+}
